ece609d191c2200c3ca4786ef6b42acd68622598
Anton Kostiuk <antkos90@gmail.com>
2025-02-16 17:22:51 +0100

    m1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,16 +18,101 @@
 //   );
 // }
 
-import { favBooks } from "./FavBooks";
-import { BookList } from "./BookList";
+// import { favBooks } from "./FavBooks";
+// import { BookList } from "./BookList";
 
 
-export default function App () {
+// export default function App () {
+//   return (
+//     <>
+// 	  <h1>Books of the week</h1>
+//       <BookList books={favBooks} />
+//     </>
+//   );
+// };
+
+
+// const alertStyles = {
+//   margin: 8,
+//   padding: "12px 16px",
+//   borderRadius: 4,
+//   backgroundColor: "gray",
+//   color: "white",
+// };
+
+
+// export default function App () {
+//   return (
+//     <>
+//       <p style={alertStyles}>Please update your email!</p>
+//       <p style={alertStyles}>There was an error during transaction!</p>
+//       <p style={alertStyles}>Payment received, thank you for your purchase!</p>
+//     </>
+//   );
+// };
+
+
+// import { Alert } from "./Alert";
+
+// export default function App () {
+//   return (
+//     <>
+//       <Alert>Please update your email!</Alert>
+//       <Alert>There was an error during transaction!</Alert>
+//       <Alert>Payment received, thank you for your purchase!</Alert>
+//     </>
+//   );
+// };
+
+// import { Alert } from "./Alert";
+
+// export default function App () {
+//   return (
+//     <>
+//       <Alert variant="info">
+//         Would you like to browse our recommended products?
+//       </Alert>
+//       <Alert variant="error">
+//         There was an error during your last transaction
+//       </Alert>
+//       <Alert variant="success">
+//         Payment received, thank you for your purchase
+//       </Alert>
+//       <Alert variant="warning">
+//         Please update your profile contact information
+//       </Alert>
+//     </>
+//   );
+// };
+
+// import { Alert } from "./Alert";
+
+
+// export default function App () {
+//   return (
+//     <>
+//       <Alert variant="info">
+//         Would you like to browse our recommended products?
+//       </Alert>
+//       <Alert variant="error" outlined>
+//         There was an error during your last transaction
+//       </Alert>
+//       <Alert variant="success" elevated>
+//         Payment received, thank you for your purchase
+//       </Alert>
+//       <Alert variant="warning" outlined elevated>
+//         Please update your profile contact information
+//       </Alert>
+//     </>
+//   );
+// };
+
+import { HiUser } from "react-icons/hi";
+
+export default function UserMenu({ name }) {
   return (
-    <>
-	  <h1>Books of the week</h1>
-      <BookList books={favBooks} />
-    </>
+    <div>
+      <p><HiUser className="my-icon" color="red" size="24" /> {name}</p>
+    </div>
   );
 };
-
