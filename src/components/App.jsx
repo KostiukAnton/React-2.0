3b09c438db984a9b0f4a5d4d0e114f22f4fdd2ed
Anton Kostiuk <antkos90@gmail.com>
2025-02-16 08:53:11 +0100

    configuration

---
+++ src/components/App.jsx
@@ -0,0 +1,33 @@
+// import Product from "./Product";
+
+// export default function App() {
+//   return (
+//     <div>
+//       <h1>Best selling</h1>
+
+//       <Product
+//         name="Tacos With Lime"
+//         price={10.99}
+//       />
+//       <Product
+//         name="Fries and Burger"
+//         imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
+//         price={14.29}
+//       />
+//     </div>
+//   );
+// }
+
+import { favBooks } from "./Favbooks";
+import { BookList } from "./BookList";
+
+
+export default function App () {
+  return (
+    <>
+	  <h1>Books of the week</h1>
+      <BookList books={favBooks} />
+    </>
+  );
+};
+
